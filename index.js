50821b42d9d71d444fceceb9970d4d7f0a0e2576
Mahir-Ahmed-250 <xahin.mahir@gmail.com>
2021-11-12 02:08:25 +0600

    Some Task Done

--- index.js
+++ index.js
@@ -22,21 +22,38 @@ async function run() {
         const database = client.db('WheelBase')
         const productCollection = database.collection('products')
         const orderCollection = database.collection('orders');
+        const reviewCollection = database.collection('reviews');
+        const userCollection = database.collection('users');
 
-        // GET API
+        // GET All Products API
         app.get('/products', async (req, res) => {
             const cursor = productCollection.find({})
             const products = await cursor.toArray()
             res.send(products)
         })
 
-
-        // GET Single Product
+        // GET Single Product API
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const product = await productCollection.findOne(query);
             res.json(product)
+        })
+
+        // POST An Product API
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result)
+        })
+
+        // DELETE Product API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.json(result)
         })
 
         // Add Orders API
@@ -52,6 +69,7 @@ async function run() {
             const services = await cursor.toArray()
             res.send(services)
         })
+
         // DELETE Orders API
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
@@ -60,6 +78,44 @@ async function run() {
             const result = await orderCollection.deleteOne(query);
             res.json(result)
         })
+
+        // POST A Review API
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
+        })
+        // GET All Reviews API
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewCollection.find({})
+            const reviews = await cursor.toArray()
+            res.send(reviews)
+        })
+
+        // ADD An User API
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await userCollection.insertOne(user);
+            res.json(result)
+        })
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        })
+
+
+
+
+
+
+
+
 
     }
     finally {
