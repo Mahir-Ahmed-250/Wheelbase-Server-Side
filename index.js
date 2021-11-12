6a62503e826ebcc3f7145abf497ae21a1b1fba75
Mahir-Ahmed-250 <xahin.mahir@gmail.com>
2021-11-13 04:25:50 +0600

    Done

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qnlpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -70,10 +68,20 @@ async function run() {
             res.send(services)
         })
 
+        //UPDATE Order Status
+        app.put('/users/approve', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const filter = { Email: user.Email, Status: user.Status };
+            const updateDoc = { $set: { Status: 'Shipped' } };
+            const result = await orderCollection.updateOne(filter, updateDoc);
+            res.json(result);
+            console.log(result)
+        })
+
         // DELETE Orders API
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id)
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
             res.json(result)
@@ -100,6 +108,7 @@ async function run() {
             res.json(result)
         })
 
+        // UPDATE/Store An User API(google)
         app.put('/users', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
@@ -109,14 +118,27 @@ async function run() {
             res.json(result)
         })
 
+        // CHECK Admin API
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'Admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
 
-
-
-
-
-
-
-
+        // UPDATE An User To Admin API
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'Admin' } };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.json(result);
+            console.log(result)
+        })
     }
     finally {
 
