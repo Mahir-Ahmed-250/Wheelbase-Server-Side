4cc46f34d0633415075c23e14d4b9e87eaaace59
unknown <xahin.mahir@gmail.com>
2022-09-12 03:39:27 +0600

    fixed

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const products = require("./Route/product");
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qnlpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -155,9 +152,6 @@ run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('Running Server')
 });
-
-app.use("/products", products);
-
 app.listen(port, () => {
     console.log('SERVER RUNNING AT', port)
 })
