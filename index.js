c508c4a952c623e7aea58882097be8b659961cc8
unknown <xahin.mahir@gmail.com>
2022-10-09 19:26:43 +0600

    test

--- index.js
+++ index.js
@@ -30,10 +30,12 @@ async function run() {
             res.send(products)
         })
 
-        app.get('/naturals', async (req, res) => {
-            const result = await toolCollection.find().toArray();
-            res.json(result);
+        app.get('/testing', async (req, res) => {
+            const cursor = testingCollection.find({})
+            const testing = await cursor.toArray()
+            res.send(testing)
         })
+
 
         // GET Single Product API
         app.get('/products/:id', async (req, res) => {
